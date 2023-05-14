8dac71e67ace1fc116106bb7b52035722714ebd9
Leonita Bahtiri <76443004+leonita-01@users.noreply.github.com>
2023-05-14 22:17:52 +0200

    Update GUIserver.py

--- GUIserver.py
+++ GUIserver.py
@@ -16,13 +16,12 @@ def run_server():
         client_socket, addr = server_socket.accept()
         log(f"Klienti i lidhur: {addr}")
 
-        # Prano mesazhin e kriptuar dhe dekriptoje
-        encrypted_message = client_socket.recv(1024)
+       
         log(f"Mesazhi i kriptuar: {encrypted_message}")
         decrypted_message = cipher_suite.decrypt(encrypted_message)
         log(f"Mesazhi i dekriptuar: {decrypted_message.decode()}")
 
-        # Dërgo mesazhin e përgjigjes të kriptuar
+      
         response = "Mesazhi u pranua."
         encrypted_response = cipher_suite.encrypt(response.encode())
         log(f"Mesazhi i kriptuar i përgjigjes: {encrypted_response}")
@@ -33,18 +32,18 @@ def run_server():
 def log(message):
     messages_listbox.insert(END, message)
 
-# Lexo certifikatën dhe çelësin privat
+
 with open('C:\\ssl\\server.crt', "rb") as cert_file:
     cert = x509.load_pem_x509_certificate(cert_file.read())
 with open('C:\\ssl\\server.key', "rb") as key_file:
     private_key = serialization.load_pem_private_key(key_file.read(), password=None)
 
-# Krijo një çelës Fernet duke përdorur çelësin publik të certifikatës
+
 key = cert.public_key().public_bytes(
     serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
-# Use HKDF to derive a 32-byte key from the public key
+
 hkdf = HKDF(
     algorithm=hashes.SHA256(),
     length=32,
@@ -55,12 +54,12 @@ hkdf = HKDF(
 derived_key = hkdf.derive(key)
 cipher_suite = Fernet(base64.urlsafe_b64encode(derived_key))
 
-# Krijo socket-in e serverit
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 12565))
 server_socket.listen(1)
 
-# Krijo GUI-në e serverit
+
 root = Tk()
 root.title("Serveri")
 
@@ -69,7 +68,7 @@ messages_listbox.pack()
 
 threading.Thread(target=run_server, daemon=True).start()
 
-# Shto mesazhin "Serveri është gati për të pritur klientë..." në GUI
+
 log("Serveri është gati për të pritur klientë...")
 
 root.mainloop()
